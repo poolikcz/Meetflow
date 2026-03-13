c661d8c66943d63962afa3e3d7d5227277464a2b
Pavel <pavel@digikiwi.cz>
2026-03-13 21:39:55 +0100

    Make HubSpot sidebar card compact

--- hubspot-project/src/app/cards/SidebarCard.tsx
+++ hubspot-project/src/app/cards/SidebarCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { EmptyState, Link, Text } from "@hubspot/ui-extensions";
+import { Flex, Illustration, Link, Stack, Text } from "@hubspot/ui-extensions";
 import { hubspot } from "@hubspot/ui-extensions";
 
 hubspot.extend<'crm.record.sidebar'>(() => <SidebarCard />);
@@ -8,17 +8,14 @@ const SidebarCard = () => {
   const calendarUrl = 'https://meetflow-woad.vercel.app';
 
   return (
-    <>
-      <EmptyState
-        title="Open Meetflow calendar"
-        layout="vertical"
-        imageName='building'
-      >
-        <Text>
-          View meetings, calls and tasks in the external calendar application.
-          <Link href={calendarUrl}> Open calendar</Link>
-        </Text>
-      </EmptyState>
-    </>
+    <Flex direction="row" align="center" gap="sm">
+      <Illustration name="meetings" alt="Meetflow" width={44} height={44} />
+
+      <Stack direction="column" distance="flush">
+        <Text format={{ fontWeight: 'demibold' }}>Meetflow calendar</Text>
+        <Text variant="microcopy">Quick access to meetings, calls and tasks.</Text>
+        <Link href={calendarUrl}>Open calendar</Link>
+      </Stack>
+    </Flex>
   );
 };
